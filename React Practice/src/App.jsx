aa85d4ff4449f0c9a5d2594a1b69cb1fd83abce2
lcortez0516 <lestercortez0516@gmail.com>
2025-02-07 14:54:59 +0800

    Learned new things

--- React Practice/src/App.jsx
+++ React Practice/src/App.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  return <div></div>;
+  const [movie, setMovie] = useState([
+    {
+      id: 1,
+      title: "Equalize1",
+      year: "1998",
+      rating: 11230,
+    },
+    {
+      id: 2,
+      title: "Equalize2",
+      year: "1928",
+      rating: 123423010,
+    },
+    {
+      id: 3,
+      title: "Equalize3",
+      year: "1798",
+      rating: 102342310,
+    },
+  ]);
+
+  const changeTitle = () => {
+    setMovie(
+      movie.map((m) => (m.id === 2 ? { ...m, title: "HOTDOG Overgrow" } : m))
+    );
+  };
+
+  return (
+    <div>
+      {movie.map((m) => (
+        <div key={m.id}>
+          <h1>{m.title}</h1>
+          <p>{m.year}</p>
+          <p>{m.rating}</p>
+        </div>
+      ))}
+
+      <button onClick={changeTitle}>Change title</button>
+    </div>
+  );
 };
 
 export default App;
