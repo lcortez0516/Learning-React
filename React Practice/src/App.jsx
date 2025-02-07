d4b297e1b35adc12021a5936c5f5ee80ce4a5245
lcortez0516 <lestercortez0516@gmail.com>
2025-02-07 20:45:30 +0800

    Update APp.jsx

--- React Practice/src/App.jsx
+++ React Practice/src/App.jsx
@@ -1,47 +1,7 @@
 import React from "react";
-import { useState } from "react";
 
 const App = () => {
-  const [movie, setMovie] = useState([
-    {
-      id: 1,
-      title: "Equalize1",
-      year: "1998",
-      rating: 11230,
-    },
-    {
-      id: 2,
-      title: "Equalize2",
-      year: "1928",
-      rating: 123423010,
-    },
-    {
-      id: 3,
-      title: "Equalize3",
-      year: "1798",
-      rating: 102342310,
-    },
-  ]);
-
-  const changeTitle = () => {
-    setMovie(
-      movie.map((m) => (m.id === 2 ? { ...m, title: "HOTDOG Overgrow" } : m))
-    );
-  };
-
-  return (
-    <div>
-      {movie.map((m) => (
-        <div key={m.id}>
-          <h1>{m.title}</h1>
-          <p>{m.year}</p>
-          <p>{m.rating}</p>
-        </div>
-      ))}
-
-      <button onClick={changeTitle}>Change title</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
