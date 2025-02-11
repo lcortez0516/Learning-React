3acd0a78fa27baa862c92a4e37779cc4541b34f2
lcortez0516 <lestercortez0516@gmail.com>
2025-02-11 18:21:31 +0800

    Deleted practice components

--- React Practice/src/App.jsx
+++ React Practice/src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { useState } from "react";
-import Modal from "./components/modal";
 
 const App = () => {
-  return (
-    <div>
-      <Modal />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
