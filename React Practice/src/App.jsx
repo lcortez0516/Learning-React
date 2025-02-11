2a95e14bd0a5089e386ef0a7ac83eb3f30d59d2e
lcortez0516 <lestercortez0516@gmail.com>
2025-02-11 20:38:20 +0800

    deleted practice codes

--- React Practice/src/App.jsx
+++ React Practice/src/App.jsx
@@ -1,11 +1,7 @@
-import Counter from "./components/Counter";
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <Counter />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- React Practice/src/components/Counter.jsx
+++ React Practice/src/components/Counter.jsx
@@ -1,52 +1,7 @@
-import React, { useState, useReducer } from "react";
-
-const initialVal = { count: 0 };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { ...state, count: state.count + action.val };
-    case "decrement":
-      return { ...state, count: state.count - action.val };
-    case "reset":
-      return { ...state, count: 0 };
-    default:
-      return state;
-  }
-};
+import React from "react";
 
 const Counter = () => {
-  const [input, setInput] = useState("");
-  const [counter, dispatch] = useReducer(reducer, initialVal);
-  return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-      />
-      <p>{counter.count}</p>
-
-      <button
-        onClick={() => {
-          console.log(setInput(parseInt(input)));
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          if (input && parseInt(input)) {
-            Number(input);
-            dispatch({ type: "increment", val: input });
-          }
-        }}
-      >
-        -
-      </button>
-      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Counter;
