af98ee3835a323d879b0c56098407b8f33869fd1
lcortez0516 <lestercortez0516@gmail.com>
2025-02-11 20:37:39 +0800

    learned Use Reducer

--- React Practice/src/App.jsx
+++ React Practice/src/App.jsx
@@ -1,23 +1,9 @@
-import React from "react";
-import ComponentA from "./components/ComponentA";
-import { createContext } from "react";
-import ComponentC from "./components/ComponentC";
-import ComponentB from "./components/ComponentB";
-
-export const nameData = new createContext();
-export const ageData = new createContext();
+import Counter from "./components/Counter";
 
 const App = () => {
-  const name = "Lester";
-  const age = 25;
   return (
     <div>
-      <nameData.Provider value={name}>
-        <ageData.Provider value={age}>
-          <ComponentC />
-          <ComponentB />
-        </ageData.Provider>
-      </nameData.Provider>
+      <Counter />
     </div>
   );
 };
