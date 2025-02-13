44bdddf6c2eddecb89f0320912c71e804a7ff834
lcortez0516 <lestercortez0516@gmail.com>
2025-02-13 21:17:14 +0800

    practiced useReducer

--- React Practice/src/components/Counter.jsx
+++ React Practice/src/components/Counter.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useReducer } from "react";
+import { initialVal, reducer } from "./Reducer.js";
 
 const Counter = () => {
-  return <div></div>;
+  const [state, dispatch] = useReducer(reducer, initialVal);
+
+  const handleIncrement = () => {
+    dispatch({ type: "increment" });
+    console.log(state.count);
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: "decrement" });
+  };
+  const handleReset = () => {
+    dispatch({ type: "reset" });
+  };
+
+  return (
+    <div>
+      <p>{state.count}</p>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleReset}>reset</button>
+    </div>
+  );
 };
 
 export default Counter;
