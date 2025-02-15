8853862e833d2e7f2b87434802c21add825f9295
lcortez0516 <lestercortez0516@gmail.com>
2025-02-15 20:02:52 +0800

    Meals App with API

---
+++ Meals-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Meals from "./components/Meals";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Meals</h1>
+      <Meals />
+    </div>
+  );
+};
+
+export default App;
